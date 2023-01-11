0ea09cbb5330d3b936881e6bae7d1a20c0bc0c0b
ruthrora <proude>
2023-01-10 16:54:57 -0800

    function that deletes the item at a specific position in a list.

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -3,7 +3,7 @@ def divisible_by_2(my_list=[]):
     a = []
     b = len(my_list)
     for iter in range(0, b):
-        if (my_list[iter] % 2) == 0:
+        if (my_list[iter] / 2) == 0:
             a.append(True)
         else:
             a.append(False)
