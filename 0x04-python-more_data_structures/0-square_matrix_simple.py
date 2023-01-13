d0474f8bafbf7a7b5c4c05c0e7c7e28937bba7d9
ruthrora <proude>
2023-01-13 11:06:11 -0800

    function that returns a set of all elements present in only one set

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    return [[matrix[a][b]**2 for j in range(len(matrix[0]))]
-            for i in range(len(matrix))]
+    if not matrix:
+        print()
+        return [[i**2 for i in row] for row in matrix]
